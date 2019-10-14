5af41bed02fcf4c1be46acc32984f01b08505f70
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-10-14 15:18:40 -0500

    updated dependencies, added test for default controls behavior

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,13 @@
-// Test away!
+
+ 
+import React from 'react';
+import { render } from '@testing-library/react';
+import Controls from './Controls';
+
+test("defaults to `unlocked` and `open`", () =>{
+    const { getByText } = render(<Controls />);
+    getByText(/Lock/i);
+    getByText(/Close/i);
+}
+)
+
