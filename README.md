2e99ed9c35a2076b9432c896134249820968123b
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-10-14 16:04:41 -0500

    testing that the open/close button is disabled if locked complete

--- README.md
+++ README.md
@@ -51,8 +51,8 @@ The expected/assumed behavior of the application is listed below.
 
 ### Gate
 
-- defaults to `unlocked` and `open`
-- cannot be closed or opened if it is locked
+- [x] defaults to `unlocked` and `open`
+- [x] cannot be closed or opened if it is locked
 
 ### Dashboard
 
--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,7 +1,7 @@
 
  
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Controls from './Controls';
 
 test("defaults to `unlocked` and `open`", () =>{
@@ -11,3 +11,12 @@ test("defaults to `unlocked` and `open`", () =>{
 }
 )
 
+test('open/close button disabled if locked', () =>{
+    const lockedMock = true;
+    const toggleClosedMock = jest.fn();
+    const { getByText } = render(
+        <Controls locked={lockedMock} toggleClosed={toggleClosedMock} />
+    );
+    fireEvent.click(getByText(/Close Gate/i));
+    expect(toggleClosedMock).toHaveBeenCalledTimes(0)
+})
